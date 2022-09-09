da4d2aa168d9617fbb46481da0a93b84b177d5e3
Ronald Jon Christopher C Bandalaria <ronaldjonchristopher.bandalaria@benilde.edu.ph>
2022-09-09 15:05:44 +0800

    Sleeping minigame can be accessed in the main scene

--- Assets/SleepingMinigame.cs
+++ Assets/SleepingMinigame.cs
@@ -7,12 +7,71 @@ public class SleepingMinigame : MinigameObject
     // Start is called before the first frame update
     void Start()
     {
-        
+        Initialize();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Initialize()
     {
-        
+        Interactable = this.GetComponent<Interactable>();
+        sceneChange = this.GetComponent<SceneChange>();
+    }
+
+    public override void Interact(GameObject player = null)
+    {
+        isInteracted = true;
+        interactRoutine = StartCoroutine(InteractCoroutine(player));
+    }
+
+    public override void EndInteract(GameObject player = null)
+    {
+        base.EndInteract(player);
+        StopCoroutine(interactRoutine);
+    }
+
+    public override void JumpToMiniGame()
+    {
+        if (sceneChange)
+        {
+            if (MinigameScene != null)
+            {
+                //Remove all listeners because when the scene changes it will destroy the scene but will try to access the old active scripts
+                Events.OnSceneChange.Invoke();
+                Events.OnInteract.RemoveListener(Interact);
+                Events.OnFinishInteract.RemoveListener(EndInteract);
+
+                sceneChange.OnChangeScene(MinigameScene);
+            }
+            else
+            {
+                Debug.Log("No next scene name");
+            }
+        }
+        else
+        {
+            Debug.Log("No Scene change");
+        }
+    }
+
+    public override IEnumerator InteractCoroutine(GameObject player = null)
+    {
+        while (isInteracted)
+        {
+            Debug.Log("Interact with" + this.gameObject.name);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                MotivationMeter playerMotivation = player.GetComponent<MotivationMeter>();
+                if (playerMotivation)
+                {
+                    playerMotivation.DecreaseMotivation(MotivationCost);
+                }
+                Debug.Log("Interacted");
+                isInteracted = false;
+                yield return new WaitForSeconds(2.0f);
+                JumpToMiniGame();
+            }
+            yield return null;
+        }
     }
 }
+
+    
