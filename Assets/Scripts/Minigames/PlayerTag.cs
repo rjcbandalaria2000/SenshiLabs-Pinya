b38938cbc45dee5dd872cb95059e31b09c70be44
Jace <jamesonandre.chiu@benilde.edu.ph>
2022-09-12 17:21:58 +0800

    Tag minigame Prototype

---
+++ Assets/Scripts/Minigames/PlayerTag.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTag : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody2D rb;
+    Vector2 movement;
+    [SerializeField] bool isTag;
+
+    [Header("Sprites")]
+    public SpriteRenderer defaultSprite;
+    public SpriteRenderer TagSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isTag = false;
+        rb = this.GetComponent<Rigidbody2D>();
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+       
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Minigames/TagMiniGame.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagMiniGame : MonoBehaviour
+{
+    public GameObject player;
+    public List<GameObject> kids;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
