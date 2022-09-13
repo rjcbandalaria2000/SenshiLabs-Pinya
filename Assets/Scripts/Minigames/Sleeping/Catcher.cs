9712cef8380f59be87d1831dfbabcdec5ffb17b1
Ronald Jon Christopher C Bandalaria <ronaldjonchristopher.bandalaria@benilde.edu.ph>
2022-09-13 09:16:57 +0800

    The collider in the basket is disable when stunned

--- Assets/Scripts/Minigames/Sleeping/Catcher.cs
+++ Assets/Scripts/Minigames/Sleeping/Catcher.cs
@@ -4,24 +4,25 @@ using UnityEngine;
 
 public class Catcher : MonoBehaviour
 {
-    public GameObject Parent; 
-    public int PlayerScore;
+    public GameObject   Parent; 
+    public int          PlayerScore;
+    public bool         CanCatch = true; 
+
     // Start is called before the first frame update
     void Start()
     {
         Parent = this.gameObject;
+        CanCatch = true;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Collided with object");
+        if (!CanCatch) { return; }
         FallingFood collidedFood = collision.gameObject.GetComponent<FallingFood>();
         if (collidedFood)
         {
             Debug.Log("Collided with" + collidedFood.name);
             collidedFood.OnCollided(Parent);
-            //SingletonManager.Get<SleepingMinigameManager>().PlayerPoints++;
-            //SingletonManager.Get<SleepingMinigameManager>().CheckIfFinished();
             Destroy(collidedFood.gameObject);   
         }
     }
--- Assets/Stun.cs
+++ Assets/Stun.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class Stun : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class Stun : MonoBehaviour
 
     private Coroutine   stunEffectRoutine;
     private MouseFollow mouseFollow;
+    private Catcher     catcher;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,27 +18,42 @@ public class Stun : MonoBehaviour
         if (Parent)
         {
             mouseFollow = Parent.GetComponent<MouseFollow>();  
+            catcher = Parent.GetComponent<Catcher>();
         }
     }
 
     IEnumerator StunUnit()
     {
-        if(mouseFollow)
+        if (mouseFollow)
         {
             Debug.Log("Cannot move");
             mouseFollow.canMove = false;
+            catcher.CanCatch = false;
             yield return new WaitForSeconds(Duration);
             mouseFollow.canMove = true;
+            catcher.CanCatch = true;
             Destroy(this);
         }
         
     }
 
     public void StartEffect()
-    {   
+    {
+        Assert.IsNotNull(Parent, "UnitTarget is null or is not set");
         mouseFollow = Parent.GetComponent<MouseFollow>();
+        catcher = Parent.GetComponent<Catcher>();
         stunEffectRoutine = StartCoroutine(StunUnit());
         
         Debug.Log("Stunned");
+    }
+
+    public void ActivateEffect(GameObject target)
+    {
+       
+    }
+
+    public void DeactivateEffect(GameObject target)
+    {
+
     }
 }
