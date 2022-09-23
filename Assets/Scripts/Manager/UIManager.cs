919381d52a199e46713fb9d60cd20a165bcd3acf
Arejay165 <arjay.diocton@gmail.com>
2022-09-23 11:29:45 +0800

    Mini Main Menu Draft Title Screen

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -143,6 +143,7 @@ public class UIManager : MonoBehaviour
 
     public void DeactivateMiniGameMainMenu()
     {
+        if (miniGameMainMenu == null) { return; }
         miniGameMainMenu.SetActive(false);
     }
 
