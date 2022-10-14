1552e60fe7bfd1261807b36fdcdf5ad95909bbdc
Ronald Jon Christopher C Bandalaria <ronaldjonchristopher.bandalaria@benilde.edu.ph>
2022-10-14 16:25:13 +0800

    Implemented Cook the food minigame in main game scene

--- Assets/Scripts/Minigames/ImHungryMinigame.cs
+++ Assets/Scripts/Minigames/ImHungryMinigame.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ImHungryMinigame : MinigameObject
 {
+    private TransitionManager transitionManager;
+
     void Start()
     {
         Initialize();
@@ -13,25 +15,42 @@ public class ImHungryMinigame : MinigameObject
     {
         interactable = this.GetComponent<Interactable>();
         sceneChange = this.GetComponent<SceneChange>();
+        transitionManager = SingletonManager.Get<TransitionManager>();
     }
 
     public override void Interact(GameObject player = null)
     {
-        if(!SingletonManager.Get<PlayerData>().IsGroceryFinished)
-        {
-            Debug.Log("Finish Pre-Req");
-            return;
-        }
+        //if(!SingletonManager.Get<PlayerData>().IsGroceryFinished)
+        //{
+        //    Debug.Log("Finish Pre-Req");
+        //    return;
+        //}
         isInteracted = true;
         MotivationMeter playerMotivation = player.GetComponent<MotivationMeter>();
         if (playerMotivation)
         {
-            playerMotivation.DecreaseMotivation(motivationCost);
+            //Check if has enough motivation
+            if (playerMotivation.MotivationAmount < motivationCost)
+            {
+                // if there is not enough motivation amount
+                Debug.Log("Not enough motivation");
+                return;
+            }
+            else
+            {
+                playerMotivation.DecreaseMotivation(motivationCost);
+                //Disable player controls 
+                PlayerControls playerControl = player.GetComponent<PlayerControls>();
+                if (playerControl)
+                {
+                    playerControl.enabled = false;
+                }
+                Debug.Log("Interacted");
+                isInteracted = true; // to avoid being called again since it is already interacted
+                StartInteractRoutine();
+            }
         }
         Debug.Log("Interacted");
-        isInteracted = false;
-        JumpToMiniGame();
-        //interactRoutine = StartCoroutine(InteractCoroutine(player));
     }
 
     public override void EndInteract(GameObject player = null)
@@ -66,7 +85,32 @@ public class ImHungryMinigame : MinigameObject
 
     public override IEnumerator InteractCoroutine(GameObject player = null)
     {
+        transitionManager = SingletonManager.Get<TransitionManager>();
+        //Disable UI Elements
+        SingletonManager.Get<UIManager>().DeactivateGameUI();
+
+        //Play animation of transition
+        if (transitionManager)
+        {
+
+            transitionManager.ChangeAnimation(TransitionManager.CURTAIN_CLOSE);
+
+        }
+        //Wait for the transition to end
+        while (!transitionManager.IsAnimationFinished())
+        {
+            Debug.Log("Closing Curtain Time: " + transitionManager.animator.GetCurrentAnimatorStateInfo(0).normalizedTime);
+            yield return null;
+        }
+
+        //Jump to next scene
+        JumpToMiniGame();
         yield return null;
     }
-
+    public override void StartInteractRoutine()
+    {
+        interactRoutine = StartCoroutine(InteractCoroutine());
+    }
 }
+
+
