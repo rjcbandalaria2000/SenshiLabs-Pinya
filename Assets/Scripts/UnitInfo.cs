7aadca18737d6450526317f86078f9fb1b9829ab
Ronald Jon Christopher C Bandalaria <ronaldjonchristopher.bandalaria@benilde.edu.ph>
2022-10-24 10:09:58 +0800

    Added Unit Info to effects gameobject to be accessed

--- Assets/Scripts/UnitInfo.cs
+++ Assets/Scripts/UnitInfo.cs
@@ -6,12 +6,17 @@ public class UnitInfo : MonoBehaviour
 {
     public GameObject   Parent;
     public GameObject   UI;
+    public GameObject   effects;
   
     public void Start()
     {
         if (Parent == null)
         {
             Parent = this.gameObject.transform.parent.gameObject;
+        }
+        if (effects)
+        {
+            effects.SetActive(false);
         }
     }
 
@@ -20,4 +25,6 @@ public class UnitInfo : MonoBehaviour
         if(Parent == null) { return null; }
         return Parent;
     }
+
+    
 }
