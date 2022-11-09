b74a41bfc2bc59a4f673fc4b07f77aecb2ab345a
Ronald Jon Christopher C Bandalaria <ronaldjonchristopher.bandalaria@benilde.edu.ph>
2022-11-09 11:24:32 +0800

    Created struct for tracking progress for each minigame

--- Assets/PlayerProgress.cs
+++ Assets/PlayerProgress.cs
@@ -2,8 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public struct ScoreTracker
+{
+    public float score;
+    public float time;
+    public int numOfTimesCompleted;
+}
+
 public class PlayerProgress : MonoBehaviour
 {
+    [Header("Tasks Trackers")]
+    public ScoreTracker cleanTheHouseTracker;
+    public ScoreTracker washTheDishesTracker;
+    public ScoreTracker groceryTracker;
+    public ScoreTracker waterThePlantsTracker;
+    public ScoreTracker getWaterTracker;
+    public ScoreTracker imHungryTracker;
+    public ScoreTracker foldTheClothesTracker;
+
+    [Header("Motivational Trackers")]
+    public ScoreTracker tagTracker;
+    public ScoreTracker sleepTracker;
+    public ScoreTracker hideSeekTracker;
+    
     private void Awake()
     {
         SingletonManager.Register(this);
@@ -13,6 +35,8 @@ public class PlayerProgress : MonoBehaviour
     {
         
     }
+
+    
 
    
 }
