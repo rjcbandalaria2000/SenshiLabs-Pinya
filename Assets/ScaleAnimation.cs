6bc29416bf34e7318c8df0e5f50e21bef53f31a1
Arejay165 <arjay.diocton@gmail.com>
2022-12-14 23:37:07 +0800

    Shake Animation

---
+++ Assets/ScaleAnimation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ScaleAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update'
+    public Vector3 startPos;
+    public Vector3 size;
+
+  
+    private void OnEnable()
+    {
+        gameObject.transform.DOShakeScale(0.5f,0.2f,2,90,true).SetLoops(-1, LoopType.Yoyo);
+        //  gameObject.transform.DOScale(size, 1f).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    
+
+    
+}
+    
