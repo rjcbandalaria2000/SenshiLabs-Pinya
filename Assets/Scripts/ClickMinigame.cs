f005b9c23138ae21353cf0c231a185d7b161f2db
Jace <jamesonandre.chiu@benilde.edu.ph>
2023-02-07 16:08:52 +0800

    OnMouseOver Effect

--- Assets/Scripts/ClickMinigame.cs
+++ Assets/Scripts/ClickMinigame.cs
@@ -7,10 +7,20 @@ public class ClickMinigame : MonoBehaviour
     public PlayerAI AIPlayer;
     public int waypointIndex;
 
+    public GameObject hoverEffect;
+    //public SpriteRenderer objSprite;
+    //public Sprite unHoverSprite;
+    //public Sprite hoverSprite;
+
     // Start is called before the first frame update
     void Start()
     {
         AIPlayer = GameObject.FindObjectOfType<PlayerAI>();
+        if(hoverEffect != null )
+        {
+            this.hoverEffect.SetActive(false);
+        }
+    
     }
 
     // Update is called once per frame
@@ -25,5 +35,16 @@ public class ClickMinigame : MonoBehaviour
       // AIPlayer.goToTarget(waypointIndex);
        StartCoroutine(AIPlayer.goToTargetRoutine(waypointIndex));
         Debug.Log("Go To Target");
+    }
+
+    private void OnMouseOver()
+    {
+        //change sprite
+        if(hoverEffect != null)
+        {
+            this.hoverEffect.SetActive(true);
+        }
+
+        Debug.Log("Mouse Over Object;");
     }
 }
