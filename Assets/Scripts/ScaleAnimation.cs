a065347f0724d2cf0af74fe680ef9c07806fa9ff
Arejay165 <arjay.diocton@gmail.com>
2023-02-16 00:04:56 +0800

    Update animation

--- Assets/Scripts/ScaleAnimation.cs
+++ Assets/Scripts/ScaleAnimation.cs
@@ -8,16 +8,28 @@ public class ScaleAnimation : MonoBehaviour
     // Start is called before the first frame update'
     public Vector3 startPos;
     public Vector3 size;
+   // public bool manualSize;
 
   
     private void OnEnable()
     {
-        gameObject.transform.DOShakeScale(1f,0.1f,0, 0,true).SetLoops(-1, LoopType.Yoyo);
-        //  gameObject.transform.DOScale(size, 1f).SetLoops(-1, LoopType.Yoyo);
+        gameObject.transform.DOShakeScale(1f, 0.1f, 0, 0, true).SetLoops(-1, LoopType.Yoyo);
+
+        //if (!manualSize)
+        //{
+
+        //}
+        //else
+        //{
+        //    gameObject.transform.DOScale(size, 1f).SetLoops(-1, LoopType.Yoyo);
+        //    //gameObject.transform.DOShakeScale(1f, 0.1f, 0, 0, true).SetLoops(-1, LoopType.Yoyo);
+        //}
     }
 
-    
+ 
 
-    
+
+
+
 }
     
